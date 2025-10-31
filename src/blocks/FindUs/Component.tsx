795ac721c391c86352de3b3bbe91765f452e0b0c
Scot MacDonald <scotmacdonald@hotmail.com>
2025-10-31 10:48:46 +0100

    api/

--- src/blocks/FindUs/Component.tsx
+++ src/blocks/FindUs/Component.tsx
@@ -42,15 +42,15 @@ export default function FindUsBlock({
   const getIconForType = (type: TransportType) => {
     switch (type) {
       case "s-bahn":
-        return secondIconUrl || "/api/media/S-Bahn-Logo.webp";
+        return secondIconUrl || "/media/S-Bahn-Logo.webp";
       case "u-bahn":
-        return thirdIconUrl || "/api/media/U-Bahn.svg";
+        return thirdIconUrl || "/media/U-Bahn.svg";
       case "tram":
-        return fifthIconUrl || "/api/media/Tram-Logo.svg";
+        return fifthIconUrl || "/media/Tram-Logo.svg";
       case "bus":
-        return sixthIconUrl || "/api/media/Bus-Logo.svg";
+        return sixthIconUrl || "/media/Bus-Logo.svg";
       default:
-        return fourthIconUrl || "/api/media/spp_logo.png";
+        return fourthIconUrl || "/media/spp_logo.png";
     }
   };
 
